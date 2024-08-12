40cc479872730fc4125c1b3fdaf76eb5a284ce8d
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2024-08-12 15:20:44 +0200

    feat: loki provider logging

--- be/internal/loki/provider.go
+++ be/internal/loki/provider.go
@@ -2,6 +2,7 @@ package loki
 
 import (
 	"fmt"
+	"log"
 	"slices"
 
 	"github.com/federico-paolillo/ssh-attempts/pkg/stats"
@@ -15,20 +16,25 @@ type Connector interface {
 
 type Provider struct {
 	connector Connector
+	log       *log.Logger
 }
 
-func NewProvider(connector Connector) *Provider {
-	return &Provider{connector}
+func NewProvider(log *log.Logger, connector Connector) *Provider {
+	return &Provider{connector, log}
 }
 
 func (p *Provider) Top15LoginAttempts(nodeName string) (stats.Attempts, error) {
 	top15QueryWithParams := fmt.Sprintf(top15QueryTemplate, nodeName)
+
+	p.log.Printf("querying loki for node '%s'. query: '%s'", nodeName, top15QueryWithParams)
 
 	rawSamples, err := p.connector.MetricQuery(top15QueryWithParams)
 
 	if err != nil {
 		return nil, fmt.Errorf("loki: could not query node %s. %w", nodeName, err)
 	}
+
+	p.log.Printf("got back from loki %d samples for node '%s'", len(rawSamples), nodeName)
 
 	result := make(stats.Attempts, 0, len(rawSamples))
 
