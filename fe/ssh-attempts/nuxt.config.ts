905df5f0fa2934c16da9d240cc5e9d9b8841d6e2
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2024-08-17 16:53:13 +0200

    feat: nuxt runtime config

--- fe/ssh-attempts/nuxt.config.ts
+++ fe/ssh-attempts/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    apiAuthHeaderKey: "<specify_in_.env>",
+    apiAuthHeaderValue: "<specify_in_.env>"
+  }
 })
