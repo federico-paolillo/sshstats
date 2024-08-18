14e0ae6a0e2a504c8d08d907951706a65d4885e4
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2024-08-18 14:33:47 +0200

    feat: new favicon

--- fe/ssh-attempts/nuxt.config.ts
+++ fe/ssh-attempts/nuxt.config.ts
@@ -1,7 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "sshstats",
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: 'favicon.png'
+        }
+      ]
+    }
+  },
   runtimeConfig: {
     apiAuthHeaderKey: "<specify_in_.env>",
     apiAuthHeaderValue: "<specify_in_.env>",
