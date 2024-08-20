4415f6149d8032c769b44735cef2d07b9aa4bea9
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2024-08-20 10:11:42 +0200

    refactor: login attempts ui

---
+++ fe/ssh-attempts/components/login-attempts.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+interface LoginAttemptsProps {
+  nodename: string
+}
+
+const props = defineProps<LoginAttemptsProps>()
+
+const { data, error } = await useLoginAttempts(props.nodename)
+
+const totalAttempts = computed(
+  () => {
+    if (data.value) {
+      return data.value.attempts.reduce((p, c) => p + c.count, 0)
+    }
+
+    return 0
+  }
+)
+</script>
+
+<template>
+  <table class="ranking">
+    <caption>
+      <span>Login attempts on node</span>
+      <span>&nbsp;</span>
+      <b>{{ nodename }}</b>
+    </caption>
+    <thead>
+      <tr>
+        <th scope="col" class="header-cell">Rank</th>
+        <th scope="col" class="header-cell">Username</th>
+        <th scope="col" class="header-cell">Attempts</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-if="data">
+        <tr v-for="(attempt, index) in data.attempts">
+          <td class="number-cell">{{ index + 1 }}</td>
+          <td class="text-cell">{{ attempt.username }}</td>
+          <td class="number-cell">{{ attempt.count }}</td>
+        </tr>
+      </template>
+      <template v-if="error">
+        <tr>
+          <td colspan="3" class="text-cell">Could not retrieve information for this node</td>
+        </tr>
+      </template>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th scope="row" class="header-cell">Total</th>
+        <td colspan="2" class="number-cell">{{ totalAttempts }}</td>
+      </tr>
+    </tfoot>
+  </table>
+</template>
+
+<style>
+.ranking {
+  width: 100%;
+  min-width: 500px;
+  table-layout: auto;
+  border-collapse: collapse;
+}
+
+.ranking th,
+.ranking td {
+  padding: 8px;
+  border: 1px solid #ddd;
+}
+
+.ranking tbody tr:nth-child(odd) {
+  background-color: #f9f9f9;
+}
+
+.ranking tbody tr:nth-child(even) {
+  background-color: #e0e0e0;
+}
+
+.ranking caption {
+  display: none;
+  text-align: left;
+}
+
+.number-cell {
+  text-align: right;
+}
+
+.text-cell {
+  text-align: left;
+}
+
+.header-cell {
+  text-align: center;
+}
+</style>
---
+++ fe/ssh-attempts/pages/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import LoginAttempts from "~/components/login-attempts.vue";
+
+const nodenames = useNodenames()
+</script>
+
+<template>
+  <section>
+    <h2>
+      Login attempts
+    </h2>
+    <div class="login-attempts-container">
+      <section class="login-attempt" v-for="nodename in nodenames">
+        <h3>Node <em>{{ nodename }}</em></h3>
+        <div class="table-scroll">
+          <LoginAttempts :nodename></LoginAttempts>
+        </div>
+      </section>
+    </div>
+  </section>
+</template>
+
+<style>
+.login-attempts-container {
+  display: flex;
+  flex-flow: row wrap;
+  gap: 8px;
+  justify-content: center;
+}
+
+.login-attempt {
+  flex: 0 1;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+}
+
+.table-scroll {
+  overflow-x: auto;
+  width: 100%;
+}
+</style>
